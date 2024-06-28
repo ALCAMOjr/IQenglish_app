dc17258d067b6d7ee92d8cf4cde348596f35e438
Alfredo <alfredocastellanoula.14@gmail.com>
2024-06-27 21:18:06 -0600

    Responsives

--- src/components/IQenglish/Dashboard.jsx
+++ src/components/IQenglish/Dashboard.jsx
@@ -129,7 +129,7 @@ function Dashboard() {
                                         </div>
                                         <ul>
                                             <li>
-                                                <a onClick={handleLogout} href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white" role="menuitem">Cerrar sesión</a>
+                                                <a onClick={handleLogout}  className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white" role="menuitem">Cerrar sesión</a>
                                             </li>
                                         </ul>
                                     </div>
